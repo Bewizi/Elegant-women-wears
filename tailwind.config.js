9fc3b7c51c195dd8b04251f1ebfaad09cf536da8
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-28 07:31:23 +0100

    Add icon components and Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        raleway: ['Raleway', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
