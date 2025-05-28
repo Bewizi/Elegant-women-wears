f7e52e67cd7dbb3c1b1cb604f7507ef8c43d78b0
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-28 08:16:06 +0100

    Integrate `Header` component into `App` template

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
 import { RouterView } from 'vue-router'
+import Header from '@/components/layouts/Header.vue'
 </script>
 
 <template>
+  <Header />
   <RouterView />
 </template>
 
