7f1ac5aa0041a2eb7a9d35d7ee98570b11b5fa53
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-28 07:33:22 +0100

    Add `HomeView` and `AboutView` components with basic structure

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import AppContainer from '@/components/AppContainer.vue'
+</script>
+
+<template>
+  <main>
+    <AppContainer class="mt-10">
+      <h1 class="font-raleway font-semibold text-7xl">Elegant Women Wears</h1>
+      <p class="font-roboto">
+        Discover carefully selected items perfect for Nigerian women aged 40-90. Comfort, quality,
+        and tradition combined.
+      </p>
+    </AppContainer>
+  </main>
+</template>
