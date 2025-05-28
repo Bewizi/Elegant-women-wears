4bcb6ea6d0311879ae63f555e3eb8d4dfc7775eb
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-28 07:30:37 +0100

    Add `AppLink` component with props and basic template

---
+++ src/components/AppLink.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+defineProps<{
+  to: string
+}>()
+</script>
+
+<template>
+  <router-link :to="to">
+    <slot />
+  </router-link>
+</template>
+
+<style scoped></style>
