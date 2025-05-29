6fe22508830090a1aa83ad406dd2027c5b5574a4
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-29 07:52:00 +0100

    Add `NotFoundView` component with message and navigation link

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import AppContainer from '@/components/AppContainer.vue'
+import AppLink from '@/components/AppLink.vue'
+</script>
+
+<template>
+  <AppContainer>
+    <h1>Page cannot be found</h1>
+    <p>Please go to the correct url</p>
+    <div>
+      <AppLink to="/">Go Back Home</AppLink>
+    </div>
+  </AppContainer>
+</template>
+
+<style scoped></style>
