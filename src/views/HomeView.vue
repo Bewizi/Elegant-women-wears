38c9f9337e701a3d390035990e810c13daf9dcb6
Toluwalase <toluwalase.bewaji@yahoo.com>
2025-07-09 09:27:17 +0100

    refactor: replace supabase data fetching with allProducts in HomeView and improve loading/error handling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,30 +4,25 @@ import HeroSection from '@/components/sections/HeroSection.vue'
 import CTASection from '@/components/sections/CTASection.vue'
 import ProductCard from '@/components/ProductCard.vue'
 import { onMounted, ref } from 'vue'
-// import { supabase } from '@/lib/supabaseClient.ts'
 import type { Product } from '@/types'
-import { httpClient } from '@/server/httpClient'
+import { allProducts } from '@/server/httpClient'
 
 const featuredProducts = ref<Product[] | null>([])
+const loading = ref<boolean>(true)
+const error = ref<string>('')
 
 onMounted(async () => {
+  loading.value = true
   try {
-    const response = await httpClient.get('/allProducts')
-    console.log('Fetched Products:', response.data)
+    const data = await allProducts()
+    console.log('Fetched Products:', data)
 
-    featuredProducts.value = response.data.slice(0, 6)
-    // debugger
-
-    // if (response) {
-    //   featuredProducts.value = response.data.slice(0, 3)
-    // }
-
-    // if (error) {
-    //   alert(error.message)
-    //   console.log('Error Fetching Data', error)
-    // }
+    featuredProducts.value = data.slice(0, 6)
   } catch (e) {
-    console.log('Error Fetching Data', e)
+    // console.log('Failed fetching all products', e)
+    error.value = `Failed to fetch products: ${e}`
+  } finally {
+    loading.value = false
   }
 })
 </script>
@@ -41,7 +36,11 @@ onMounted(async () => {
       <div>
         <h2 class="text-3xl font-bold mb-12 text-center text-gray-800">Featured Products</h2>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div v-if="loading" class="text-center py-12">Loading Products ...</div>
+
+        <div v-else-if="error">{{ error }}</div>
+
+        <div v-else class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           <ProductCard v-for="product in featuredProducts" :key="product.id" :product="product" />
         </div>
       </div>
