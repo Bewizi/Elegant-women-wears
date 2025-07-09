4d83e3467f29e17e1649edbc8e3244d1a0fdcc47
Toluwalase <toluwalase.bewaji@yahoo.com>
2025-07-09 08:47:25 +0100

    refactor: update httpClient configuration with dynamic baseURL and response interceptors

--- src/server/httpClient.ts
+++ src/server/httpClient.ts
@@ -1,5 +1,32 @@
+// import { onMounted } from 'vue'
 import axios from 'axios'
 
 export const httpClient = axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    // 'Access-Control-Allow-Origin': '*',
+    // 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    // 'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  },
 })
+
+httpClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error('Axios Error:', error.response)
+    return Promise.reject(error)
+  },
+)
+
+// onMounted(async () => {
+//   try {
+//     const res = await httpClient.get('/products')
+//     console.log(res)
+//     res.data.slice(3, 0)
+//   } catch (e) {
+//     console.error('Error fetching products:', e)
+//   }
+// })
